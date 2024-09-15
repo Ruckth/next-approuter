fdbe99615b4db113ae544bf4d3408d69a74dde67
Kritsakorn <6431503077@lamduan.mfu.ac.th>
2024-09-15 13:11:08 +0700

    Top nav and image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,23 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="bg-gray-800 p-4 border-b-white">
+    <div> Gallery</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
+    <html lang="en">
+    <body>
+     <TopNav />
+      {children}
+    </body>
+  </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Link from "next/link";
 
+// mockup urls
+const mockupUrls = [
+ "https://utfs.io/f/YuAp8ZPhEJz216AJyUNEmJM97LlkxTui2ANR6pUPtjqBhvso",
+  "https://utfs.io/f/YuAp8ZPhEJz2LQoflGX75r0RNdJo1qsUOk4jifBuLwlg8Dyc",
+  "https://utfs.io/f/YuAp8ZPhEJz2hvLJKlbjOECAZkJe89NlRuo13sIqL6YPpyFm",
+  "https://utfs.io/f/YuAp8ZPhEJz2XZZmvVYsL2scmPZ5MaNIWnS6rQyEfYB4hKl7",
+];
+
+const mockImages =  mockupUrls.map((url,index) => ({
+  id: index + 1 ,
+  url,
+}));
+
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hi
+    <main className="text-center">
+      Hi (Images)
+      <div className="justify-center flex flex-wrap gap-3">
+      {[...mockImages,...mockImages,...mockImages].map((image) => (
+        <div key={image.id} className="w-48">
+        <img src={image.url} />
+        </div>
+      ))}
+      </div>
     </main>
   );
 }
